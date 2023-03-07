f1198f3c35ea9548559c49366319a8dcace04fd9
ANASS NASSIRI <nassirri115@gmail.com>
2023-03-07 16:48:48 +0100

    merge sort algorith

---
+++ merge_sort.py
@@ -0,0 +1,9 @@
+def merge_sort(list):
+    """
+    Sorts list in ascending order
+    Returns a new sorted list
+    Divide:Find the midpoint of the list nd divide into sublists
+    Conquer:Recursively sort the sublists created in previous step
+    Combine: Merge the sorted sublists created in previous step
+
+    """
