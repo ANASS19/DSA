c7db04c858ef0a24b699204345791041e7bb79d5
ANASS NASSIRI <nassirri115@gmail.com>
2023-03-08 21:16:57 +0100

    dsa

--- merge_sort.py
+++ merge_sort.py
@@ -7,3 +7,49 @@ def merge_sort(list):
     Combine: Merge the sorted sublists created in previous step
 
     """
+    if len(list) <=1:
+        return list
+    left_half,right_half=split(list)
+    left=merge_sort(left_half)
+    right=merge_sort(right_half)
+    return merge(left, right)
+
+def split(list):
+    """
+    Divide the unsorted list at midpoint into sublists
+    Return two sublists - left and right
+    """
+    mid=len(list)//2
+    left=list[:mid]
+    right=list[mid:]
+
+    return left,right
+
+def merge(left,right):
+    """
+    Merges two lists(arrays),sorting them in the proces
+    Returns a new merged list
+    """
+    l=[]
+    i=0
+    j=0
+
+    while i< len(left) and j<len(right):
+        if left[i]<right[j]:
+            l.append(left[i])
+            i+=1
+        else:
+            l.append(right[j])
+            j+=1
+    while i<len(left):
+        l.append(left[i])
+        i+=1
+    while j<len(right):
+        l.append(right[j])
+        j+=1
+    
+    return l
+
+alist=[54,87,23,65,97,15,432,48,1]
+l=merge_sort(alist)
+print(l)
